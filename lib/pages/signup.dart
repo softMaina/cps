eedb44d500399ca648d31d1bc77daacba0beac25
softMaina <allancodes@gmail.com>
2022-06-18 21:49:20 +0300

    [Feature] add design for the signup page

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -5,10 +5,67 @@ class SignUp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign Up screen'),
-      ),
+    return  Scaffold(
+        body: Center(
+          child: Card(
+            margin: const EdgeInsets.all(0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: const Text(
+                      'SIGN UP',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Roboto',
+                          fontSize: 25),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Email'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Username'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Password'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                          foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black)),
+                      onPressed: () => {},
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 
